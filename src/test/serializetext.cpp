435534cde89dacd59bbaa821acd9a77b5bdabf6d
Mike Buland <mike@xagasoft.com>
2006-05-13 02:16:06 +0000

    Fixed a bug in the tester.

--- src/test/serializetext.cpp
+++ src/test/serializetext.cpp
@@ -1,24 +1,27 @@
 #include "serializertext.h"
+#include "staticstring.h"
+#include <iostream>
 
 int main()
 {
+	StaticString s("You're a dog!!");
 	SerializerText ar("hello.dat", false);
 
-	ar << 4;
-	ar << 3.993;
-	ar << true;
+	ar << 4 << 3.993 << true << s;
 
 	ar.close();
 
-	int one=0;float two=0.0;bool three=false;
+	int one=0;float two=0.0;bool three=false; s = "";
 
 	SerializerText ar2("hello.dat", true);
 	
 	ar2 >> one;
 	ar2 >> two;
 	ar2 >> three;
+	ar2 >> s;
 	
-	printf("out: %d, %d, %s\n", one, two, three);
+	//printf("out: %d, %f, %s, \"%s\"\n", one, two, (three ? "true" : "false"), s.getString());
+	std::cout << one << ", " << two << ", " << three << ", " << s.getString() << "\n";
 	
 	return 0;
 }
