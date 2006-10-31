6fdb8bca29c4887d78bb426cfad507b597012a4c
Mike Buland <mike@xagasoft.com>
2006-10-31 05:41:13 +0000

    Added an sha1 class...

---
+++ src/sha1.h
@@ -0,0 +1,42 @@
+/* sha1.h
+
+Copyright (c) 2005 Michael D. Leonhard
+
+http://tamale.net/
+
+This file is licensed under the terms described in the
+accompanying LICENSE file.
+*/
+
+#ifndef SHA1_H
+#define SHA1_H
+
+#include <stdint.h>
+
+/**
+ * Calculates SHA-1 sums.  This is based strongly on code from Michael D.
+ * Leonhard who released his code under the terms of the MIT license, thank you!
+ */
+class Sha1
+{
+public:
+	Sha1();
+	~Sha1();
+
+	void update( const char* data, int num );
+	unsigned char* getDigest();
+
+	// utility methods
+	static uint32_t lrot( uint32_t x, int bits );
+	static void toBigEndian( uint32_t in, unsigned char* out );
+
+private:
+	// fields
+	uint32_t H0, H1, H2, H3, H4;
+	unsigned char bytes[64];
+	int unprocessedBytes;
+	uint32_t size;
+	void process();
+};
+
+#endif
---
+++ src/tests/sha1.cpp
@@ -0,0 +1,44 @@
+#include "sha1.h"
+#include "file.h"
+
+#define BS 	1024
+
+int main( int argc, char *argv[] )
+{
+	argc--; argv++;
+
+	if( argc == 0 )
+	{
+		printf("Provide a filename.\n");
+		return 0;
+	}
+
+	char buf[BS];
+
+	Sha1 s;
+	File fin( *argv, "rb" );
+	for(;;)
+	{
+		int nRead = fin.read( buf, BS );
+		if( nRead == 0 )
+			break;
+
+		s.update( buf, nRead );
+		if( nRead < BS )
+			break;
+	}
+
+	unsigned char *dig = s.getDigest();
+
+	char val[]={"0123456789ABCDEF"};
+
+	for( int j = 0; j < 20; j++ )
+	{
+		putchar( val[dig[j]>>4] );
+		putchar( val[dig[j]&0x0F] );
+	}
+	putchar('\n');
+
+	delete[] dig;
+}
+
