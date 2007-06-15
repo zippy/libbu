b6e6bc55731c42f9336feea378f4b0fb6fbfe056
David Braden V <neonphog@xagasoft.com>
2007-06-15 18:54:59 +0000

    david - wrote two silly unit tests...

---
+++ src/unit/hash.cpp
@@ -0,0 +1,40 @@
+#include "bu/fstring.h"
+#include "bu/hash.h"
+#include "unitsuite.h"
+
+#include <stdio.h>
+
+class Unit : public Bu::UnitSuite
+{
+private:
+	typedef Bu::Hash<Bu::FString, int> StrIntHash;
+public:
+	Unit()
+	{
+		setName("Hash");
+		addTest( Unit::test_probe );
+	}
+
+	virtual ~Unit()
+	{
+	}
+
+	void test_probe()
+	{
+		StrIntHash h;
+		char buf[20];
+		for(int i=0;i<10000;i++)
+		{
+			sprintf(buf,"%d",i);
+			Bu::FString sTmp(buf);
+			h[sTmp] = i;
+			unitTest( h.has(sTmp) );
+		}
+	}
+};
+
+int main( int argc, char *argv[] )
+{
+	return Unit().run( argc, argv );
+}
+
---
+++ src/unit/taf.cpp
@@ -0,0 +1,48 @@
+#include "unitsuite.h"
+#include "file.h"
+#include "tafreader.h"
+
+#include <string.h>
+#include <unistd.h>
+
+class Unit : public Bu::UnitSuite
+{
+public:
+	Unit()
+	{
+		setName("taf");
+		addTest( Unit::read1 );
+	}
+
+	virtual ~Unit()
+	{
+	}
+
+	void read1()
+	{
+#define FN_TMP ("/tmp/tmpXXXXXXXX")
+		Bu::FString sFnTmp(FN_TMP);
+		Bu::File fOut = Bu::File::tempFile( sFnTmp, "wb" );
+		const char *data =
+"{test: name=\"Bob\"}"
+;
+		fOut.write(data,strlen(data));
+		fOut.close();
+
+		Bu::File fIn(sFnTmp.c_str(), "rb");
+		Bu::TafReader tr(fIn);
+		
+		Bu::TafNode *tn = tr.getNode();
+		unitTest( !strcmp("Bob", tn->getProperty("name").c_str()) );
+		delete tn;
+
+		unlink(sFnTmp.c_str());
+#undef FN_TMP
+	}
+};
+
+int main( int argc, char *argv[] )
+{
+	return Unit().run( argc, argv );
+}
+
