99e5e61ffc5cab1376495fb7e38bd3b28eef0d53
Mike Buland <mike@xagasoft.com>
2007-10-14 03:37:42 +0000

    Added a temlatized Queue skeleton class, but I really didn't feel like writing it just now.  The Queue class has some obvious advantages over list if you really only need Queue functionality, the main one being size, but also you can get a slight speed advantage.

---
+++ src/queue.h
@@ -0,0 +1,16 @@
+#ifndef BU_QUEUE_H
+#define BU_QUEUE_H
+
+namespace Bu
+{
+	template<typename value, typename valuealloc = std::allocator<value> >
+	class Queue
+	{
+	public:
+
+	private:
+
+	};
+}
+
+#endif
