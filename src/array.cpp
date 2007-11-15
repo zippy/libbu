b2196471577b8d2ddca31904cc1c56decc027e95
Mike Buland <mike@xagasoft.com>
2007-11-15 21:32:16 +0000

    Added liscense info at the front of many, many files.  Debating the old files.

--- src/array.cpp
+++ src/array.cpp
@@ -1,2 +1,9 @@
+/*
+ * Copyright (C) 2007 Xagasoft, All rights reserved.
+ *
+ * This file is part of the libbu++ library and is released under the
+ * terms of the license contained in the file LICENSE.
+ */
+
 #include "bu/array.h"
 
--- src/tests/formula.cpp
+++ src/tests/formula.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2007 Xagasoft, All rights reserved.
+ *
+ * This file is part of the libbu++ library and is released under the
+ * terms of the license contained in the file LICENSE.
+ */
+
 #include "bu/formula.h"
 
 int main( int argc, char *argv[] )
