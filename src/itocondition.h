b44e6f6a0918cbfc023f7fcd4d653fa12ec66506
David Braden V <neonphog@xagasoft.com>
2007-11-15 21:31:43 +0000

    david - adding license tag to the top of all header files

--- src/itocondition.h
+++ src/itocondition.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2007 Xagasoft, All rights reserved.
+ *
+ * This file is part of the libbu++ library and is released under the
+ * terms of the license contained in the file LICENSE.
+ */
+
 #ifndef BU_ITO_CONDITION_H
 #define BU_ITO_CONDITION_H
 
