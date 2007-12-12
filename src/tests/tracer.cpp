79ccdfbd86be0563ef4351db2e8ad13dcbddb922
Mike Buland <mike@xagasoft.com>
2007-12-12 14:45:05 +0000

    Added the new TRACE test.

---
+++ src/tests/tracer.cpp
@@ -0,0 +1,28 @@
+
+#define BU_TRACE
+#include "bu/trace.h"
+
+void doThing3( int x, const char *bob, void *p )
+{
+	TRACE( x, bob, p );
+}
+
+void doThing2( int x, const char *bob )
+{
+	TRACE( x, bob );
+}
+
+void doThing( int x )
+{
+	TRACE( x );
+}
+
+int main( int argc, char *argv[] )
+{
+	doThing( 54 );
+	doThing2( 128, "Hello" );
+	doThing3( 266, "Goodbye", argv );
+
+	return 0;
+}
+
