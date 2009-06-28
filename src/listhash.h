1ad328ed3d4d5f75515c921693a4578f0de73bf7
Mike Buland <mike@xagasoft.com>
2009-06-28 20:48:47 +0000

    Added the Bu::ListHash class, it's a really simple wrapper for Bu::Hash that puts all of the values in a Bu::List assosiated with the key.  When you insert, it will append to the list at that key, and create a list if it needs to.
    When erasing, getting, etc, you operate on the whole list, not just one element.

---
+++ src/listhash.h
@@ -0,0 +1,47 @@
+#ifndef BU_LIST_HASH_H
+#define BU_LIST_HASH_H
+
+#include "bu/hash.h"
+#include "bu/list.h"
+
+namespace Bu
+{
+	template<typename key, typename value, typename sizecalc = __calcNextTSize_fast, typename keyalloc = std::allocator<key>, typename valuealloc = std::allocator<Bu::List<value> >, typename challoc = std::allocator<uint32_t> >
+	class ListHash : public Hash<key, Bu::List<value>, sizecalc, keyalloc, valuealloc, challoc>
+	{
+		typedef Hash<key, Bu::List<value>, sizecalc, keyalloc, valuealloc, challoc> ParentType;
+	public:
+		ListHash()
+		{
+		}
+
+		ListHash( const ListHash &src ) :
+			ParentType( src )
+		{
+		}
+
+		virtual ~ListHash()
+		{
+		}
+
+		ListHash &operator=( const ListHash &src )
+		{
+			*dynamic_cast<ParentType *>(this) =
+				dynamic_cast<ParentType &>(src);
+		}
+
+		virtual void insert( const key &k, const value &v )
+		{
+			if( !has( k ) )
+			{
+				ParentType::insert( k, List<value>() );
+			}
+			get( k ).append( v );
+		}
+		
+	private:
+	};
+
+};
+
+#endif
