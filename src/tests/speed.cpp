75c46a59e98c6805086f7555123d7857a713fdef
Mike Buland <mike@xagasoft.com>
2009-08-14 12:48:21 +0000

    A good start on a speed test framework.

---
+++ src/tests/speed.cpp
@@ -0,0 +1,48 @@
+#include "bu/fstring.h"
+#include <sys/time.h>
+
+template<typename a>
+struct tstCopy
+{
+	tstCopy( const a &src ) :
+		src( src )
+	{
+	}
+
+	void operator()()
+	{
+		a tmp = src;
+	}
+
+	a src;
+};
+
+template<typename f>
+double runTest( f fnc )
+{
+	struct timeval tStart, tEnd;
+	int j = 0;
+	gettimeofday( &tStart, NULL );
+	for(; j < 500000; j++ )
+		fnc();
+	gettimeofday( &tEnd, NULL );
+
+	return (double)(tEnd.tv_sec-tStart.tv_sec)+
+		(double)(tEnd.tv_usec-tStart.tv_usec)/1000000.0;
+}
+
+template<typename tst>
+void fullTest( tst t )
+{
+	double dTotal;
+	int iCount = 10;
+	for( int j = 0; j < iCount; j++ )
+		dTotal += runTest( t );
+	printf("Average time:  %f\n", dTotal/iCount );
+}
+
+int main()
+{
+	fullTest( tstCopy<Bu::FString>("This is a test string.") );
+}
+
