b1199fcc334bc51c7da9fd4aa728ae7c90fe027d
Mike Buland <mike@xagasoft.com>
2009-08-22 04:00:54 +0000

    Pretty much just removed the debugging from SharedCore, it wasn't at fault, and it passed many more valgrind tests.

--- src/sharedcore.h
+++ src/sharedcore.h
@@ -13,17 +13,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void hardlog(const char *fmt, ...);
-
-void fncin( void *base, const char *fn, void *core, int *refcnt, int params, ... );
-void fncout( void *base, const char *fn, void *core, int *refcnt, int params, ... );
-
-
-#define fin( count, ... ) fncin( (void *)this, __FUNCTION__, core, iRefCount, count, ##__VA_ARGS__ )
-#define fout( count, ... ) fncout( (void *)this, __FUNCTION__, core, iRefCount, count, ##__VA_ARGS__ )
-
-#define rc  ((iRefCount==NULL)?(-1):(*iRefCount))
-
 namespace Bu
 {
 	template<typename Core>
@@ -35,33 +24,25 @@ namespace Bu
 			core( NULL ),
 			iRefCount( NULL )
 		{
-			fin( 0 );
 			core = _allocateCore();
 			iRefCount = new int(1);
-			fout( 0 );
 		}
 
 		SharedCore( const _SharedType &rSrc ) :
 			core( NULL ),
 			iRefCount( NULL )
 		{
-			fin( 1, &rSrc );
 			_softCopy( rSrc );
-			fout( 1, &rSrc );
 		}
 
 		virtual ~SharedCore()
 		{
-			fin( 0 );
 			_deref();
-			fout( 0 );
 		}
 
 		SharedCore &operator=( const SharedCore &rhs )
 		{
-			fin( 1, &rhs );
 			_softCopy( rhs );
-			fout( 1, &rhs );
 			return *this;
 		}
 
@@ -74,43 +55,34 @@ namespace Bu
 		Core *core;
 		void _hardCopy()
 		{
-			fin( 0 );
 			if( !core || !iRefCount )
-			{	fout( 0 );	return; }
+				return;
 			if( (*iRefCount) == 1 )
-			{	fout( 0 );	return; }
+				return;
 			Core *copy = _copyCore( core );
 			_deref();
 			core = copy;
 			iRefCount = new int( 1 );
-			fout( 0 );
 		}
 
 		virtual Core *_allocateCore()
 		{
-			fin( 0 );
-			fout( 0 );
 			return new Core();
 		}
 
 		virtual Core *_copyCore( Core *pSrc )
 		{
-			fin( 0 );
-			fout( 0 );
 			return new Core( *pSrc );
 		}
 
 		virtual void _deallocateCore( Core *pSrc )
 		{
-			fin( 0 );
-			fout( 0 );
 			delete pSrc;
 		}
 
 	private:
 		void _deref()
 		{
-			fin( 0 );
 			if( (--(*iRefCount)) == 0 )
 			{
 				_deallocateCore( core );
@@ -118,26 +90,21 @@ namespace Bu
 			}
 			core = NULL;
 			iRefCount = NULL;
-			fout( 0 );
 		}
 
 		void _incRefCount()
 		{
-			fin( 0 );
 			if( iRefCount && core )
 				++(*iRefCount);
-			fout( 0 );
 		}
 
 		void _softCopy( const _SharedType &rSrc )
 		{
-			fin( 1, &rSrc );
 			if( core )
 				_deref();
 			core = rSrc.core;
 			iRefCount = rSrc.iRefCount;
 			_incRefCount();
-			fout( 1, &rSrc );
 		}
 
 		int *iRefCount;
