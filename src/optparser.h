8a45ebf4c70027031eaa8ab0bb297bc73639d968
Mike Buland <mike@xagasoft.com>
2010-05-13 14:10:31 +0000

    Added license info to the top of the remaining files.  I should add a check for that to the build file or something...

--- src/optparser.h
+++ src/optparser.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2007-2010 Xagasoft, All rights reserved.
+ *
+ * This file is part of the libbu++ library and is released under the
+ * terms of the license contained in the file LICENSE.
+ */
+
 #ifndef BU_OPT_PARSER_H
 #define BU_OPT_PARSER_H
 
--- src/tests/daysinmonth.cpp
+++ src/tests/daysinmonth.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2007-2010 Xagasoft, All rights reserved.
+ *
+ * This file is part of the libbu++ library and is released under the
+ * terms of the license contained in the file LICENSE.
+ */
+
 #include "bu/sio.h"
 #include "bu/util.h"
 
