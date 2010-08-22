bf65e33edb9b6b04a34fd829cacd3e3487cced8c
Mike Buland <mike@xagasoft.com>
2010-08-22 05:34:12 +0000

    Bu::StreamStack works, it's tested, reasonably, it will be used first in the gats project in Gats::ProtocolGats.

--- src/streamstack.cpp
+++ src/streamstack.cpp
@@ -4,83 +4,203 @@ Bu::StreamStack::StreamStack()
 {
 }
 
+Bu::StreamStack::StreamStack( Bu::Stream *pStream )
+{
+	lFilts.prepend( pStream );
+}
+
 Bu::StreamStack::~StreamStack()
 {
+	clearStack();
+}
+
+bool Bu::StreamStack::isEmpty()
+{
+	return lFilts.isEmpty();
+}
+
+bool Bu::StreamStack::hasStream()
+{
+	return !lFilts.isEmpty();
+}
+
+void Bu::StreamStack::setStream( Bu::Stream *pStream )
+{
+	if( !lFilts.isEmpty() )
+		throw Bu::ExceptionBase("There is already a stream set.");
+
+	lFilts.prepend( pStream );
+}
+
+void Bu::StreamStack::clearStack()
+{
+	for( FilterList::iterator i = lFilts.begin(); i; i++ )
+	{
+		delete *i;
+	}
+
+	lFilts.clear();
+}
+
+void Bu::StreamStack::popFilter()
+{
+	if( lFilts.isEmpty() )
+		return;
+
+	delete lFilts.first();
+	lFilts.erase( lFilts.begin() );
+}
+
+Bu::Stream *Bu::StreamStack::getTop()
+{
+	checkStack();
+
+	return lFilts.first();
+}
+
+Bu::Stream *Bu::StreamStack::getStream()
+{
+	checkStack();
+
+	return lFilts.last();
 }
 
 void Bu::StreamStack::close()
 {
+	checkStack();
+
+	lFilts.first()->close();
 }
 
 size_t Bu::StreamStack::read( void *pBuf, size_t nBytes )
 {
+	checkStack();
+
+	return lFilts.first()->read( pBuf, nBytes );
 }
 
 size_t Bu::StreamStack::write( const void *pBuf, size_t nBytes )
 {
+	checkStack();
+
+	return lFilts.first()->write( pBuf, nBytes );
 }
 
 size_t Bu::StreamStack::write( const Bu::FString &sBuf )
 {
+	checkStack();
+
+	return lFilts.first()->write( sBuf );
 }
 
 long Bu::StreamStack::tell()
 {
+	checkStack();
+
+	return lFilts.first()->tell();
 }
 
 void Bu::StreamStack::seek( long offset )
 {
+	checkStack();
+
+	lFilts.first()->seek( offset );
 }
 
 void Bu::StreamStack::setPos( long pos )
 {
+	checkStack();
+
+	lFilts.first()->setPos( pos );
 }
 
 void Bu::StreamStack::setPosEnd( long pos )
 {
+	checkStack();
+
+	lFilts.first()->setPosEnd( pos );
 }
 
 bool Bu::StreamStack::isEos()
 {
+	checkStack();
+
+	return lFilts.first()->isEos();
 }
 
 bool Bu::StreamStack::isOpen()
 {
+	checkStack();
+
+	return lFilts.first()->isOpen();
 }
 
 void Bu::StreamStack::flush()
 {
+	checkStack();
+
+	lFilts.first()->flush();
 }
 
 bool Bu::StreamStack::canRead()
 {
+	checkStack();
+
+	return lFilts.first()->canRead();
 }
 
 bool Bu::StreamStack::canWrite()
 {
+	checkStack();
+
+	return lFilts.first()->canWrite();
 }
 
 bool Bu::StreamStack::isReadable()
 {
+	checkStack();
+
+	return lFilts.first()->isReadable();
 }
 
 bool Bu::StreamStack::isWritable()
 {
+	checkStack();
+
+	return lFilts.first()->isWritable();
 }
 
 bool Bu::StreamStack::isSeekable()
 {
+	checkStack();
+
+	return lFilts.first()->isSeekable();
 }
 
 bool Bu::StreamStack::isBlocking()
 {
+	checkStack();
+
+	return lFilts.first()->isBlocking();
 }
 
 void Bu::StreamStack::setBlocking( bool bBlocking )
 {
+	checkStack();
+
+	lFilts.first()->setBlocking( bBlocking );
 }
 
 void Bu::StreamStack::setSize( long iSize )
 {
+	checkStack();
+
+	lFilts.first()->setSize( iSize );
+}
+
+inline void Bu::StreamStack::checkStack()
+{
+	if( lFilts.isEmpty() )
+		throw Bu::ExceptionBase("StreamStack is empty.");
 }
 
--- src/streamstack.h
+++ src/streamstack.h
@@ -3,14 +3,99 @@
 
 #include "bu/stream.h"
 
+#include <typeinfo>
+
 namespace Bu
 {
 	class StreamStack : public Bu::Stream
 	{
+	private:
+		typedef Bu::List<Bu::Stream *> FilterList;
+
 	public:
 		StreamStack();
+		StreamStack( Bu::Stream *pStream );
 		virtual ~StreamStack();
 
+		bool isEmpty();
+		bool hasStream();
+		void setStream( Bu::Stream *pStream );
+
+		void clearStack();
+		void popFilter();
+		Bu::Stream *getTop();
+
+		Bu::Stream *getStream();
+
+		template<typename filter>
+		Bu::Stream *findFilter()
+		{
+			for( FilterList::iterator i = lFilts.begin(); i; i++ )
+			{
+				if( typeid(**i) == typeid( filter ) )
+				{
+					return *i;
+				}
+			}
+
+			throw Bu::ExceptionBase("Filter not found.");
+		}
+
+		template<typename filter>
+		void pushFilter()
+		{
+			checkStack();
+
+			filter *pFlt = new filter( *lFilts.first() );
+			lFilts.prepend( pFlt );
+		}
+
+		template<typename filter, typename p1t>
+		void pushFilter( p1t p1 )
+		{
+			checkStack();
+
+			filter *pFlt = new filter( *lFilts.first(), p1 );
+			lFilts.prepend( pFlt );
+		}
+
+		template<typename filter, typename p1t, typename p2t>
+		void pushFilter( p1t p1, p2t p2 )
+		{
+			checkStack();
+
+			filter *pFlt = new filter( *lFilts.first(), p1, p2 );
+			lFilts.prepend( pFlt );
+		}
+
+		template<typename filter, typename p1t, typename p2t, typename p3t>
+		void pushFilter( p1t p1, p2t p2, p3t p3 )
+		{
+			checkStack();
+
+			filter *pFlt = new filter( *lFilts.first(), p1, p2, p3 );
+			lFilts.prepend( pFlt );
+		}
+
+		template<typename filter, typename p1t, typename p2t, typename p3t,
+			typename p4t>
+		void pushFilter( p1t p1, p2t p2, p3t p3, p4t p4 )
+		{
+			checkStack();
+
+			filter *pFlt = new filter( *lFilts.first(), p1, p2, p3, p4 );
+			lFilts.prepend( pFlt );
+		}
+
+		template<typename filter, typename p1t, typename p2t, typename p3t,
+			typename p4t, typename p5t>
+		void pushFilter( p1t p1, p2t p2, p3t p3, p4t p4, p5t p5 )
+		{
+			checkStack();
+
+			filter *pFlt = new filter( *lFilts.first(), p1, p2, p3, p4, p5 );
+			lFilts.prepend( pFlt );
+		}
 
 		//
 		// Everything below here merely passes on the call to the top of the
@@ -39,7 +124,9 @@ namespace Bu
 		virtual void setSize( long iSize );
 
 	private:
-		typedef Bu::List<Bu::Stream *> FilterList;
+		void checkStack();
+
+	private:
 		FilterList lFilts;
 	};
 };
