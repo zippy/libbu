074d5ac7015ff2f5ebb6452c4118544e219d1cdd
Mike Buland <mike@xagasoft.com>
2011-04-04 03:12:32 +0000

    Really just made some decisions about the overall functionality of the UtfString and now I'm ready to put some more of the basics into action.

--- src/utfstring.h
+++ src/utfstring.h
@@ -12,21 +12,40 @@
 
 namespace Bu
 {
-	class String;
+	/**
+	 * UtfChar isn't actually a character, unicode specifies "code points" not
+	 * characters.  The main reason for this is that not all code points define
+	 * usable characters.  Some control text directionality, some apply
+	 * properties to other code points which are characters.  However, most of
+	 * these distinctions are only important when implementing displays that
+	 * comply with the Unicode standard fully.
+	 */
+	typedef uint32_t UtfChar;
 
 	class UtfString
 	{
 	public:
-		UtfString();
-		virtual ~UtfString();
+		enum Encoding
+		{
+			Utf8,
+			Utf16,
+			Utf16be,
+			Utf16le,
+			Utf32,
+			Ucs16,
+			GuessEncoding
+		};
 
-		typedef uint32_t point;
+		UtfString();
+		UtfString( const Bu::String &sInput, Encoding eEnc=Utf8 );
+		virtual ~UtfString();
 
 		static void debugUtf8( const Bu::String &sUtf8 );
 
 	private:
-//		typedef BasicString<uint16_t> RawString;
-//		RawString rsStore;
+		uint16_t *pData;
+		int iRawLen;
+		int iCharLen;
 	};
 };
 
